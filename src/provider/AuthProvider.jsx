0e4457d2684c11a67b47aa4f55d8dc8ef2d76cb5
monzurul002 <monzurul002@gmail.com>
2023-10-24 15:49:27 +0600

    authProvider ended

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,21 +1,62 @@
 import { createContext } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"; import app from "../firebase/firebase.config";
-;
+import { getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile, sendPasswordResetEmail, signOut, GithubAuthProvider } from "firebase/auth"; import app from "../firebase/firebase.config";
+
 
 
 export const AuthContext = createContext(null);
 const auth = getAuth(app)
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider = ({ children }) => {
     const googleProvider = new GoogleAuthProvider;
+    const githubProvider = new GithubAuthProvider;
+
+    //google signin
     const googleSignIn = () => {
         return signInWithPopup(auth, googleProvider)
     }
 
+    //createUser
+    const createUserWithEmailPassword = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // password signIn
+    const emailPasswordSignIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    //update Profile
+    const userProfileUpdate = (name, photoURL) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photoURL
+        })
+    }
+    //reset password
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email)
+    }
+    // logout
+    const logOut = () => {
+        return signOut(auth)
+    }
+    //github area
+    const signInWithGitHub = () => {
+        return signInWithPopup(auth, githubProvider)
+    }
+
+
 
     const authInfo = {
-        displayName: "pasnat",
-        googleSignIn
+
+        googleSignIn,
+        emailPasswordSignIn,
+        createUserWithEmailPassword,
+        userProfileUpdate,
+        resetPassword,
+        logOut,
+        signInWithGitHub
     }
 
 
