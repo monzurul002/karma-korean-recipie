266ca67aa1f983ae1bd4973d748158f33ad52ea0
monzurul002 <monzurul002@gmail.com>
2023-10-29 11:08:16 +0600

    newslater added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import Slider from "./Carousel/Slider";
 import bg from "../../assets/carousel/blacgBg.jpg"
 import Chef from "../Chef/Chef";
 import RecipieDetails from "./recipieDetails";
+import NewsLater from "./NewsLater";
 const Home = () => {
     return (
         <div className="py-5 " style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover" }}>
             <Slider></Slider>
             <Chef></Chef>
             <RecipieDetails></RecipieDetails>
+            <NewsLater></NewsLater>
         </div>
     );
 };
---
+++ src/pages/Home/NewsLater.jsx
@@ -0,0 +1,27 @@
+import { Form } from "react-bootstrap";
+import emailBg from '../../assets/emailBg.png'
+
+const NewsLater = () => {
+    return (
+        <div style={{ backgroundImage: `url(${emailBg})`, filter: "opacity(85%)" }} className=" p-5 container">
+            <div className="container bg-white p-5 w-50 my-5 mx-auto">
+                <div className="text-center">
+                    <h2 className="text-primary-emphasis text-bold">News Latter</h2>
+                    <h6 className="text-secondary">Get update subscribe for 0$/week</h6>
+                </div>
+                <Form>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Your comments</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default NewsLater;
